a7e6f2213bd99806bbd111edca843aa81e86a4c1
Aolin Yang <aolin.yang315@gmail.com>
2018-02-21 17:55:15 -0600

    Add files via upload

--- pythonSearchEngine-master/test2.py
+++ pythonSearchEngine-master/test2.py
@@ -3,18 +3,41 @@
 import imp
 import sys
 imp.reload(sys)
-#sys.setdefaultencoding('utf8')
+import pandas as pd
 
-# f = open("testdata.txt","r")
-# print (f)
+# Load csv
+df = pd.read_csv("testexcel.csv") 
+
+#print(df.get("artist.name"))
+
+
+#get artist name
+def getArtistNameByIndex (idx):
+	print ("get '{}' at index {}".format(df.at[idx,"artist.name"], idx))
+	return df.at[idx,"artist.name"]
+
+
+def getArtistNameByIndex2 (idx):
+	print ("get '{}' at index {}".format(df.sort_values(by=["artist.name"]).at[idx,"artist.name"], idx))
+	return df.at[idx,"artist.name"]
+
+
+#sort by column name
+def sort_by (ColName):
+	print("sorted by {}".format(ColName))
+	return df.sort_values(by=[ColName])
+
+
+def getRecordByArtistName (artistName):
+	print(df.loc[df['artist.name'] == artistName])
+	return df.loc[df['artist.name'] == artistName]
 
 
 def searching (line, target):
 	for word in line.split(' '):
 		if word == target:
 			print ("found "+ target)
-			return True
-	
+			return True	
 
 def runcode ():
 	count = 0
@@ -28,5 +51,16 @@ def runcode ():
 	print ("searching {} words".format(count))
 	print ("found target {} times".format(found))
 
+def testRun():
+	print(df.at[0,"artist.name"])
+	#getArtistNameByIndex(0)
+	#print(sort_by("artist.name"))
+	#print(sort_by("artist.name").at[0,"artist.name"])
+	getArtistNameByIndex2(0)
+	#print(sort_by("artist.name").at[0,"artist.name"])
+	
+
+
+testRun()
 
 #runcode()
