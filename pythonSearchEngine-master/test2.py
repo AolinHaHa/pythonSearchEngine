1922f34ee4545663fa8c243e30b5037431cf8a01
Aolin Yang <aolin.yang315@gmail.com>
2018-02-28 15:12:38 -0600

    added functions
    getArtistTF
    getAName
    GroupArtistMbtags
    stopwords

--- pythonSearchEngine-master/test2.py
+++ pythonSearchEngine-master/test2.py
@@ -14,22 +14,6 @@ tf = 0
 idf = 0
 totalCol = df.shape[0]
 allRec = []
-# 	singleRec.append(ii)
-# allRec.append(singleRec)
-
-# def getallRec():
-# 	global allRec
-	# singleRec = []
-for i in f:
-	for ii in i.split(','):
-		allRec.append(ii)
-# return allRec
-temp = []
-for idx in range(len(allRec)):
-	temp.append(allRec[idx])
-	idx +=34
-	
-print(temp)
 
 
 def getAName(lst):
@@ -37,10 +21,6 @@ def getAName(lst):
 	for idx in range(len(lst)):
 		temp.append(lst[idx])
 		idx +=34
-# 	return temp
-# print(getAName(allRec))
-
-
 
 
 #get artist name
@@ -64,6 +44,7 @@ def searching (line, target):
 		if word == target:
 			print ("found "+ target)
 			return True	
+
 def countTerm(term):
 	return
 
@@ -71,18 +52,13 @@ def tfidf (term):
 	global tf
 	count = 0
 	df.loc[df['title'] == term]
-	# for line in f.readlines():
-	# 	searching (line, "was")
-	# 	if searching (line, "was") == True:
-	# 		TF += 1
-	# 	for word in line:
-	# 		count += 1
 	print ("searching {} words".format(count))
 	print ("found target {} times".format(TF))
 
-def GroupArtistMbtags():
+def GroupArtistMbtags(): #grouy by mbtags, get number of mbtags and number of artist/ group by keys
 	lst = []
-	print(df.groupby('artist_mbtags_count').artist_mbtags_count.count())
+	print("Groupby mbtags\n",df.groupby('artist_mbtags_count').artist_mbtags_count.count())
+	print("Groupby keys\n")
 	xx = df.groupby('key').key.count()
 	for item in xx:
 		lst.append(item)
@@ -90,8 +66,19 @@ def GroupArtistMbtags():
 
 
 
-def PrintArtistCount():
-	print(df['artist.name'].value_counts())
+def getArtistTF(ArtistName): ##return artist term frequency
+	tf = 0
+	for i in df['artist.name'] == ArtistName :
+		if i == True:
+			tf += 1
+	print (tf)
+	return tf
+
+def getGeneralTF ():
+	return
+
+
+
 
 def testRun():
 	global totalCol
@@ -103,11 +90,13 @@ def testRun():
 	print(totalCol)
 	# GroupArtistMbtags()
 	a = sorted ("artist.name")
+	getArtistTF('Casual')
 	# print(a.at[0,"artist.name"])
 
 
+def removeQueryStopwords(query):#remove the stop words
+	stopwords = [ "I", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has", "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" ]
+	filteredQuery = [word for word in query.split(" ") if word.lower() not in stopwords]
+	# print(filteredQuery)
+	return filteredQuery
 
-
-#testRun()
-# for i in df:
-# 	for ii in i.split(' ')
